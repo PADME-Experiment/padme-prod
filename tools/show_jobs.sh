2d2b76ddee34bfada8c6a8e6431ac12b37333c8b
Emanuele Leonardi <emanuele.leonardi@roma1.infn.it>
2019-10-11 11:44:46 +0200

    Updated tools for new features

--- tools/show_jobs.sh
+++ tools/show_jobs.sh
@@ -12,23 +12,31 @@ SELECT
   p.name                             AS production,
   j.name                             AS job,
   CASE j.status WHEN 0 THEN '0 Created'
-                WHEN 1 THEN '1 Processing'
+                WHEN 1 THEN '1 Active'
                 WHEN 2 THEN '2 Success'
                 WHEN 3 THEN '3 Fail'
                 ELSE 'Unknown'
   END                                AS 'job status',
   LPAD(FORMAT(j.n_events,0),8," ")   AS events,
   s.submit_index                     AS 'sub idx',
-  CASE s.status WHEN 0 THEN '0 Created'
-                WHEN 1 THEN '1 Submitted'
-                WHEN 2 THEN '2 Running'
-                WHEN 3 THEN '3 Done'
-                WHEN 4 THEN '4 Fail'
-                WHEN 5 THEN '5 Done - Bad'
-                WHEN 6 THEN '6 Fail - Bad'
-                WHEN 7 THEN '7 Cancelled'
-                WHEN 8 THEN '8 Undefined'
-                ELSE 'Unknown'
+  CASE s.status WHEN   0 THEN '  0 Created'
+                WHEN   1 THEN '  1 Registered'
+                WHEN   2 THEN '  2 Pending'
+                WHEN   3 THEN '  3 Idle'
+                WHEN   4 THEN '  4 Running'
+                WHEN   5 THEN '  5 Really-Running'
+                WHEN   6 THEN '  6 Held'
+                WHEN   7 THEN '  7 Done-OK'
+                WHEN   8 THEN '  8 Done-Failed'
+                WHEN   9 THEN '  9 Cancelled'
+                WHEN  10 THEN ' 10 Aborted'
+                WHEN  11 THEN ' 11 Unknown'
+                WHEN  12 THEN ' 12 Undef'
+                WHEN 107 THEN '107 Done-OK - No Out'
+                WHEN 108 THEN '108 Done-Failed - No Out'
+                WHEN 109 THEN '109 Cancelled - No Out'
+                WHEN 207 THEN '207 Done-OK - RC!=0'
+                         ELSE '??? Unknown'
   END                                AS 'sub status',
   s.ce_job_id                        AS 'ce job id',
   s.description                      AS 'description',
