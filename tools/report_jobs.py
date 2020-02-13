20cc0bc39008ebe65d08ab0cba15a784e793ecb4
Emanuele Leonardi <emanuele.leonardi@roma1.infn.it>
2020-02-13 15:25:39 +0100

    Modified report_jobs to include only jobs from known productions

--- tools/report_jobs.py
+++ tools/report_jobs.py
@@ -1,22 +1,63 @@
 #!/usr/bin/python
 
 import sys
+import os
 import subprocess
 import re
 import shlex
+import MySQLdb
 
-def print_job(jobid,status,exitcode,description,workernode,localuser):
-    user = "%s@%s"%(localuser,workernode)
-    print "%-50s %-34s %-15s"%(jobid,user,status),
-    if exitcode:    print "ExitCode %s"%exitcode,
-    if description: print "'%s'"%description,
-    print
-
+# List of endpoints to check
 endpoints = [
     "atlasce1.lnf.infn.it:8443",
     "atlasce2.lnf.infn.it:8443",
-    "atlasce4.lnf.infn.it:8443"
+    "atlasce4.lnf.infn.it:8443",
+    "ce04-lcg.cr.cnaf.infn.it:8443"
 ]
+
+# Get DB connection parameters from environment variables
+DB_HOST   = os.getenv('PADME_MCDB_HOST'  ,'percona.lnf.infn.it')
+DB_PORT   = int(os.getenv('PADME_MCDB_PORT'  ,'3306'))
+DB_USER   = os.getenv('PADME_MCDB_USER'  ,'padmeMCDB')
+DB_PASSWD = os.getenv('PADME_MCDB_PASSWD','unknown')
+DB_NAME   = os.getenv('PADME_MCDB_NAME'  ,'PadmeMCDB')
+
+# Connect to database
+try:
+    conn = MySQLdb.connect(host   = DB_HOST,
+                           port   = DB_PORT,
+                           user   = DB_USER,
+                           passwd = DB_PASSWD,
+                           db     = DB_NAME)
+except:
+    print "*** ERROR *** Unable to connect to DB. Exception: %s"%sys.exc_info()[0]
+    sys.exit(2)
+c = conn.cursor()
+
+def job_production(jobid):
+    sql_code = """
+SELECT p.name
+FROM job_submit s
+  INNER JOIN job j ON s.job_id=j.id
+  INNER JOIN production p ON j.production_id=p.id
+WHERE s.ce_job_id='%s'
+"""%jobid
+    c.execute(sql_code)
+    res = c.fetchone()
+    conn.commit()
+    if res == None: return ""
+    return res[0]
+
+def print_job(jobid,status,exitcode,description,workernode,localuser):
+    prod = job_production(jobid)
+    if prod != "":
+        user = "%s@%s"%(localuser,workernode)
+        print "%-30s %-55s %-50s %-15s"%(prod,jobid,user,status),
+        if exitcode:    print "ExitCode %s"%exitcode,
+        if description: print "'%s'"%description,
+        print
+
+### Main program starts here ###
 
 for endpoint in endpoints:
 
